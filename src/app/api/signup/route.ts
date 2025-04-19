d24d964fb1343bc6f2a440f6b921c61cbae4ef6c
Carlos De Santiago <carlosddes@gmail.com>
2025-04-19 15:03:26 -0400

    created signup route

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,21 @@
+import connectMongoDB from "../../../../lib/mongodb";
+import User from "@/models/UserSchema";
+import { NextResponse, NextRequest } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: NextRequest) {
+    await connectMongoDB();
+    const { email, password } = await request.json();
+    const hashedPassword = await bcrypt.hash(password, 5);
+    const newUser = new User({
+        email,
+        password: hashedPassword
+    });
+
+    try {
+        await User.create(newUser);
+        return NextResponse.json({ message: "User created successfully" }, { status: 201 });
+    } catch (error: any) {
+        return NextResponse.json({ message: "Error creating user" }, { status: 400 });
+    }
+}

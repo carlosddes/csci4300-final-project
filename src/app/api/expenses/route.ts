e29f3d926e73bf1c2d98514ccb2261f8de1fab00
Garrett Plumlee <garrett.plumleee@gmail.com>
2025-04-20 17:14:03 -0400

    All User routes, all GET routes, all DELETE routes

--- src/app/api/expenses/route.ts
+++ src/app/api/expenses/route.ts
@@ -1,5 +1,11 @@
-import connectMongoDB from "mongodb";
+import connectMongoDB from "../../../../lib/mongodb";
 import Expense from "@models/expenseSchema"
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import mongoose from "mongoose";
+
+export async function GET(request: NextRequest) {
+    await connectMongoDB();
+    const expenses = await Expense.find();
+    return NextResponse.json({expenses});
+}

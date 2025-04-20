f15d4fd94d41c7edf7aa90914db3fad4c567ca76
Carlos De Santiago <carlosddes@gmail.com>
2025-04-19 23:31:35 -0400

    Created transaction view component

--- src/components/ui/IncomeExpenseItem.tsx
+++ src/components/ui/IncomeExpenseItem.tsx
@@ -1,6 +1,7 @@
 import { Transaction } from "@/types/types";
 import { useEffect, useState } from "react";
 import EditComponent from "./EditComponent";
+import ViewTransactionComponent from "./ViewTransactionComponent";
 
 interface IncomeExpenseItemProps {
     transaction: Transaction;
@@ -9,17 +10,22 @@ interface IncomeExpenseItemProps {
 const IncomeExpenseItem = ( {transaction}: IncomeExpenseItemProps) => {
 
     const[isEditVisible, setIsEditVisible] = useState(false);
+    const[isViewVisible, setIsViewVisible] = useState(false);
 
     useEffect(() => {
-        if (isEditVisible) {
+        if (isEditVisible || isViewVisible) {
             document.body.style.overflow = 'hidden';
         } else {
             document.body.style.overflow = 'auto';
         }
-    }, [isEditVisible])
+    }, [isEditVisible, isViewVisible])
 
     function toggleEditComponent() {
         setIsEditVisible(!isEditVisible);
+    }
+
+    function toggleViewComponent() {
+        setIsViewVisible(!isViewVisible);
     }
 
     return (
@@ -31,15 +37,16 @@ const IncomeExpenseItem = ( {transaction}: IncomeExpenseItemProps) => {
                     <p className="font-sans text-gray-500 font-med">{transaction.date}</p>
                     <p className="font-sans text-black font-semibold">{transaction.amount}</p>
                     <div className="flex flex-row gap-8">
-                        <button className="bg-blue-200 rounded-md w-12">View</button>
+                        <button className="bg-blue-200 rounded-md w-12" onClick={toggleViewComponent}>View</button>
                         <button className="bg-green-200 rounded-md w-12" onClick={toggleEditComponent}>Edit</button>
                         <button className="bg-red-200 rounded-md w-14">Delete</button>
                     </div>
                 </div>
             </div>
             <div>
-                { isEditVisible && <div className="bg-black opacity-75 w-screen h-screen top-0 left-0 fixed"></div> }
+                { (isEditVisible || isViewVisible) && <div className="bg-black opacity-75 w-screen h-screen top-0 left-0 fixed"></div> }
                 { isEditVisible && <EditComponent editedTransaction={transaction} closeFunction={toggleEditComponent}/> }
+                { isViewVisible && <ViewTransactionComponent transaction={transaction} closeFunction={toggleViewComponent}/>}
             </div>
         </div>
     );

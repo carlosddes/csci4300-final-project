205de66d7dd02050eff0cd9df200297dcb8a48f2
Carlos De Santiago <carlosddes@gmail.com>
2025-04-19 23:44:50 -0400

    Created transaction delete

--- src/components/ui/IncomeExpenseItem.tsx
+++ src/components/ui/IncomeExpenseItem.tsx
@@ -2,6 +2,7 @@ import { Transaction } from "@/types/types";
 import { useEffect, useState } from "react";
 import EditComponent from "./EditComponent";
 import ViewTransactionComponent from "./ViewTransactionComponent";
+import ConfirmDelete from "./ConfirmDelete";
 
 interface IncomeExpenseItemProps {
     transaction: Transaction;
@@ -11,14 +12,15 @@ const IncomeExpenseItem = ( {transaction}: IncomeExpenseItemProps) => {
 
     const[isEditVisible, setIsEditVisible] = useState(false);
     const[isViewVisible, setIsViewVisible] = useState(false);
+    const[isDeleteVisible, setIsDeleteVisible] = useState(false);
 
     useEffect(() => {
-        if (isEditVisible || isViewVisible) {
+        if (isEditVisible || isViewVisible || isDeleteVisible) {
             document.body.style.overflow = 'hidden';
         } else {
             document.body.style.overflow = 'auto';
         }
-    }, [isEditVisible, isViewVisible])
+    }, [isEditVisible, isViewVisible, isDeleteVisible])
 
     function toggleEditComponent() {
         setIsEditVisible(!isEditVisible);
@@ -26,6 +28,10 @@ const IncomeExpenseItem = ( {transaction}: IncomeExpenseItemProps) => {
 
     function toggleViewComponent() {
         setIsViewVisible(!isViewVisible);
+    }
+
+    function toggleDeleteComponent() {
+        setIsDeleteVisible(!isDeleteVisible);
     }
 
     return (
@@ -39,14 +45,15 @@ const IncomeExpenseItem = ( {transaction}: IncomeExpenseItemProps) => {
                     <div className="flex flex-row gap-8">
                         <button className="bg-blue-200 rounded-md w-12" onClick={toggleViewComponent}>View</button>
                         <button className="bg-green-200 rounded-md w-12" onClick={toggleEditComponent}>Edit</button>
-                        <button className="bg-red-200 rounded-md w-14">Delete</button>
+                        <button className="bg-red-200 rounded-md w-14" onClick={toggleDeleteComponent}>Delete</button>
                     </div>
                 </div>
             </div>
             <div>
-                { (isEditVisible || isViewVisible) && <div className="bg-black opacity-75 w-screen h-screen top-0 left-0 fixed"></div> }
+                { (isEditVisible || isViewVisible || isDeleteVisible) && <div className="bg-black opacity-75 w-screen h-screen top-0 left-0 fixed"></div> }
                 { isEditVisible && <EditComponent editedTransaction={transaction} closeFunction={toggleEditComponent}/> }
                 { isViewVisible && <ViewTransactionComponent transaction={transaction} closeFunction={toggleViewComponent}/>}
+                { isDeleteVisible && <ConfirmDelete transaction={transaction} closeFunction={toggleDeleteComponent}/>}
             </div>
         </div>
     );

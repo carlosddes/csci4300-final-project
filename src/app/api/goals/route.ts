6618fd6d10b9b1828b54e428a5584a9a7c88ce09
garrett-plumlee <gap48709@uga.edu>
2025-04-21 15:26:41 -0400

    CRUD Routes Complete
    POST and PUT added. Also edited a schema typo

--- src/app/api/goals/route.ts
+++ src/app/api/goals/route.ts
@@ -1,10 +1,14 @@
 import connectMongoDB from "../../../../lib/mongodb";
-import Goal from "@models/goalsSchema"
+import Goal from "@/models/GoalsSchema"
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-import mongoose from "mongoose";
 
-// Missing POST
+export async function POST(request: NextRequest) {
+    const {amount, userID} = await request.json();
+    await connectMongoDB();
+    await Goal.create({amount, userID});
+    return NextResponse.json({messsage: "Goal added succesfully"}, {status: 201});
+}
 
 export async function GET(request: NextRequest) {
     await connectMongoDB();
--- src/app/api/incomes/route.ts
+++ src/app/api/incomes/route.ts
@@ -1,10 +1,14 @@
 import connectMongoDB from "../../../../lib/mongodb";
-import Income from "@models/incomeSchema"
+import Income from "@/models/IncomeSchema"
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-import mongoose from "mongoose";
 
-// Missing POST
+export async function POST(request: NextRequest) {
+    const {title, description, imageURL, amount, date, paymentMethod, userID} = await request.json();
+    await connectMongoDB();
+    await Income.create({title, description, imageURL, amount, date, paymentMethod, userID});
+    return NextResponse.json({messsage: "Income added succesfully"}, {status: 201});
+}
 
 export async function GET(request: NextRequest) {
     await connectMongoDB();

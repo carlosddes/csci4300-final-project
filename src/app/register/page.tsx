eca23d44d07f40abe7546c4d6d60ebecb33e5ff7
Carlos De Santiago <carlosddes@gmail.com>
2025-04-22 21:31:44 -0400

    edited signup page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -16,15 +16,21 @@ function LoginForm() {
 
     try {
       const formData = new FormData(event.currentTarget);
-      const response = await doCredentialLogin(formData);
-
-      console.log(response);
-
-      if (response?.error) {      
-        console.error(response.error);
-        setError(response.error.message || "An error occurred");
+      
+      if (formData.get("password") != formData.get("confirmPassword")) {
+        setError("Passwords do not match")
       } else {
-        router.push("/overview");
+        const url = "http://localhost:3000/api/signup";
+        const res = await fetch(url, {
+          method: "POST",
+          headers: { "Content-Type": "application/json"},
+          body: JSON.stringify({name: formData.get("name"), email: formData.get("email"), password: formData.get("password")})
+        });
+        if (res.status === 201) {
+          router.push("/login");
+        } else {
+          setError("Email already in use")
+        }
       }
     } catch (e: any) {
       console.error(e);
@@ -34,13 +40,24 @@ function LoginForm() {
   }
 
   return (
-    <div className="w-full md:w-[400px] p-10">
+    <div className="flex flex-col m-auto w-full md:w-[400px] p-10">
       <h2 className="text-xl text-gray-600 mb-2">Welcome to</h2>
       <h1 className="text-2xl font-bold text-blue-600 mb-6">
         Your Own Personal<br />Finance Tracker
       </h1>
 
       <form onSubmit={e => onSubmit(e)}>
+
+      <div className="mb-4">
+          <label className="text-gray-600 text-sm">Name</label>
+          <input
+            name="name"
+            type="text"
+            placeholder=""
+            className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+        </div>
+
         <div className="mb-4">
           <label className="text-gray-600 text-sm">Email</label>
           <input
@@ -64,36 +81,26 @@ function LoginForm() {
         </div>
 
         <div className="mb-4">
-          <label className="text-gray-600 text-sm">Confirm Email</label>
-          <input
-            name="email"
-            type="email"
-            placeholder=""
-            className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </div>
-
-        <div className="mb-4">
           <label className="text-gray-600 text-sm">Confirm Password</label>
           <div className="relative">
             <input
-              name="password"
+              name="confirmPassword"
               type="password"
               placeholder=""
               className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
           </div>
         </div>
-        <div className="flex justify-between items-center text-sm text-gray-600 mb-6">
-
-        </div>
-        <p className='text-blue-500 hover:underline'>{error}</p>
+        <p className='text-red-500 mb-2 text-center hover:underline'>{error}</p>
         <button
             type="submit"
             className="block w-full text-center bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
             >
             Create Account
         </button>
+        <div className="text-center">
+          <Link className="text-blue-600 font-medium hover:underline text-sm mt-4" href={"/login"}>Have an account?{' '}</Link>   
+         </div>  
       </form>
     </div>
   );
--- src/models/UserSchema.ts
+++ src/models/UserSchema.ts
@@ -7,7 +7,7 @@ export interface IUser extends Document {
 }
 
 const userSchema = new Schema<IUser>({
-    name: { type: String, required: true, unique: true},
+    name: { type: String, required: true},
     email: { type: String, required: true, unique: true},
     password: { type:String, required: true}
 });
